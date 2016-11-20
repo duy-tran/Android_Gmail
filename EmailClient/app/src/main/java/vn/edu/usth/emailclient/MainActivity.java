3df06ff2dbd980a93adab1682a7040116b541410
Duy <grimshady.z@gmail.com>
2016-11-20 16:45:11 +0700

    Initiate Write mail

--- EmailClient/app/src/main/java/vn/edu/usth/emailclient/MainActivity.java
+++ EmailClient/app/src/main/java/vn/edu/usth/emailclient/MainActivity.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.emailclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(getApplicationContext(), WriteMailActivity.class);
+                getApplicationContext().startActivity(myIntent);
             }
         });
 
@@ -81,7 +82,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_inbox) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_sent) {
 
         } else if (id == R.id.nav_draft) {
---
+++ EmailClient/app/src/main/java/vn/edu/usth/emailclient/WriteMailActivity.java
@@ -0,0 +1,11 @@
+package vn.edu.usth.emailclient;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by duy on 11/20/16.
+ */
+
+public class WriteMailActivity extends AppCompatActivity {
+
+}
