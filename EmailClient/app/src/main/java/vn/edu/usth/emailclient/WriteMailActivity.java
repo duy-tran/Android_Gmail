8bc8aca050da04a085791f69066c96818570ca8b
Duy <grimshady.z@gmail.com>
2016-11-20 21:43:32 +0700

    Back button on new mail activity using parent activity

--- EmailClient/app/src/main/java/vn/edu/usth/emailclient/WriteMailActivity.java
+++ EmailClient/app/src/main/java/vn/edu/usth/emailclient/WriteMailActivity.java
@@ -1,7 +1,9 @@
 package vn.edu.usth.emailclient;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,9 +16,18 @@ public class WriteMailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setTitle("Compose new email");
         setContentView(R.layout.activity_writemail);
-        Log.i("WriteMail","CREATED");
+
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.writemail_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
