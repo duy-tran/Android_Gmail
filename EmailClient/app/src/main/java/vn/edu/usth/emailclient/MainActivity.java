f07e98532c2723d799002a939e63e0bee0f998af
Duy <grimshady.z@gmail.com>
2016-11-20 15:36:20 +0700

    Add launcher icon, modify nav menu, add refresh button

--- EmailClient/app/src/main/java/vn/edu/usth/emailclient/MainActivity.java
+++ EmailClient/app/src/main/java/vn/edu/usth/emailclient/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inbox) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_sent) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_draft) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_spam) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_trash) {
 
         }
 
