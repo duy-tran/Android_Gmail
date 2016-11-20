7585bff54b163ac93cc9c2c6ef46676d154d5570
Duy <grimshady.z@gmail.com>
2016-11-20 18:30:43 +0700

    Create xml for writemail, remove title bar

--- EmailClient/app/src/main/java/vn/edu/usth/emailclient/WriteMailActivity.java
+++ EmailClient/app/src/main/java/vn/edu/usth/emailclient/WriteMailActivity.java
@@ -1,11 +1,46 @@
 package vn.edu.usth.emailclient;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by duy on 11/20/16.
  */
 
 public class WriteMailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_writemail);
+        Log.i("WriteMail","CREATED");
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
 }
